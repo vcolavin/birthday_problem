d4e8a9fd993f617f85d02d2f4e11639430820187
vcolavin <vcolavin@gmail.com>
2016-08-10 12:34:29 -0700

    all the DOM stuff is in place

--- birthday_problem.js
+++ birthday_problem.js
@@ -1,1 +1,19 @@
-console.log("hey coolguy")
+// source for the math
+// http://mathworld.wolfram.com/BirthdayProblem.html
+
+document.addEventListener('DOMContentLoaded', function(){
+  document.getElementById('submit').onclick = function(event) {
+    event.preventDefault()
+    var numberOfDays = document.getElementById('days-in-year').value
+    var numberOfPeople = document.getElementById('number-of-people').value
+
+    var probability = calculateProbability(numberOfDays, numberOfPeople);
+
+    document.getElementById('answer').style.display = 'block'
+    document.getElementById('percent').innerHTML = probability
+  }
+});
+
+function calculateProbability(days, people) {
+  return 6;
+}
