c3257f80154d8caa5fd2a3041ed21612201e40fd
vcolavin <vcolavin@gmail.com>
2016-08-10 12:41:00 -0700

    clean up organization

--- birthday_problem.js
+++ birthday_problem.js
@@ -2,17 +2,21 @@
 // http://mathworld.wolfram.com/BirthdayProblem.html
 
 document.addEventListener('DOMContentLoaded', function(){
-  document.getElementById('submit').onclick = function(event) {
-    event.preventDefault()
-    var numberOfDays = document.getElementById('days-in-year').value
-    var numberOfPeople = document.getElementById('number-of-people').value
+  document.getElementById('submit').onclick = submitClick;
+});
 
-    var probability = calculateProbability(numberOfDays, numberOfPeople);
+function submitClick(event) {
+  event.preventDefault()
+  var numberOfDays = document.getElementById('days-in-year').value
+  var numberOfPeople = document.getElementById('number-of-people').value
 
+  var probability = calculateProbability(numberOfDays, numberOfPeople);
+
+  if (numberOfDays && numberOfPeople) {
     document.getElementById('answer').style.display = 'block'
     document.getElementById('percent').innerHTML = probability
   }
-});
+}
 
 function calculateProbability(days, people) {
   return 6;
