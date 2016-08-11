f7959aca91c802670c72a276f6be4decb47a041a
vcolavin <vcolavin@gmail.com>
2016-08-10 20:12:06 -0700

    add approximate calculation

--- birthday_problem.js
+++ birthday_problem.js
@@ -1,47 +1,65 @@
 document.addEventListener('DOMContentLoaded', function(){
   math.config({precision: 2000});
-  document.getElementById('submit').onclick = submitClick;
+  document.getElementById('submit-precise').onclick = onCalcClick;
+  document.getElementById('submit-approx').onclick = onCalcClick;
 });
 
-function submitClick(event) {
-  event.preventDefault()
-  var numberOfPeople = parseInt(document.getElementById('number-of-people').value)
-  var numberOfDays = parseInt(document.getElementById('days-in-year').value)
+function onCalcClick(event) {
+  event.preventDefault();
+  var numberOfPeople = parseInt(document.getElementById('number-of-people').value);
+  var numberOfDays = parseInt(document.getElementById('days-in-year').value);
 
-  var displayString = ""
-  var probability = 0
+  var displayString = "";
+  var probability = 0;
 
   if (numberOfPeople > numberOfDays) {
-    probability = 1
+    probability = 1;
   }
   else if (numberOfDays && numberOfPeople) {
-    probability = calculateProbability(numberOfPeople, numberOfDays)
+    if (event.target.value === "precise") {
+      probability = calculatePreciseProbability(numberOfPeople, numberOfDays);
+    }
+    else if (event.target.value === "approximate") {
+      probability = calculateApproximateProbability(numberOfPeople, numberOfDays)
+    }
   }
 
-  displayString = buildDisplayString(probability)
+  displayString = buildDisplayString(probability);
 
-  document.getElementById('answer').style.display = 'block'
-  document.getElementById('answer').innerHTML = displayString
+  document.getElementById('answer').style.display = 'block';
+  document.getElementById('answer').innerHTML = displayString;
 }
 
 // source:
 // http://mathworld.wolfram.com/BirthdayProblem.html
-// essentially, P(people, days) = 1 - (d!/((d-n)!) * d^n)
-function calculateProbability(n, d) {
+// 1 - (d!/((d-n)!) * d^n)
+function calculatePreciseProbability(n, d) {
 
-  var numerator = math.factorial(math.bignumber(d))
+  var numerator = math.factorial(math.bignumber(d));
 
   var denominator = (
     // note: the docs say "multiply", not "times"
     math.factorial(math.bignumber(d - n)).times(math.pow(d, n))
   )
 
-  var q = math.divide(numerator, denominator)
+  var q = math.divide(numerator, denominator);
+  var p = math.subtract(1, q);
+
+  return math.round(p, 100);
+}
+
+// source:
+// http://mathworld.wolfram.com/BirthdayProblem.html
+// 1 - (1 - (n / 2d)^(n-1))
+function calculateApproximateProbability(n, d) {
+  var innerVal = math.bignumber(1 - (n / (2 * d)))
+
+  var q = math.pow(innerVal, n - 1)
   var p = math.subtract(1, q)
 
-  return math.round(p, 100)
+  return math.round(p, 100);
 }
 
 function buildDisplayString(probability) {
-  return "Probability two people share a birthday: " + probability
+  return "Probability two people share a birthday: " + probability;
 }
